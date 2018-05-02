09e8c847d79783164bf1fe771d11cde7553c4500
Administrator <Administrator@2012-20121002PX>
2018-05-02 21:02:43 +0800

    init project

---
+++ java01/src/java01/z.java
@@ -0,0 +1,9 @@
+package java01;
+
+public class z {
+	public static void main(String[] args) {
+		System.out.println("ÎÒ×î°ô");
+		System.out.println("");
+	}
+
+}
---
+++ java04/src/java04/x.java
@@ -0,0 +1,15 @@
+package java04;
+
+public class x {
+	static int age;//
+	static long along;//0
+	static double adouble;//0.0
+	static char c;
+	static boolean aboolean;//false
+	
+	public static void main(String[] args) {
+		System.out.println(age+"/n"+along+"/n"+adouble+"/n"+c+"/n"+aboolean);
+	}
+
+}
+
